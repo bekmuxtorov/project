c67f833d110045e09a4d6328de418030fd4fc3d7
bekmuxtorov <asadbekmuxtorov2@gmail.com>
2023-08-12 22:10:38 +0500

    📤 Updated account admin.py

--- account/admin.py
+++ account/admin.py
@@ -1,10 +1,51 @@
 from django.contrib import admin
-from .models import Account
+from .models import Account, Region
 
 
 @admin.register(Account)
 class AccountAdmin(admin.ModelAdmin):
-    list_display = ('username', 'full_name', 'id', 'type', 'phone_number', 'working_region')
-    ordering = ('created_at', 'full_name', 'id')
+    list_display = ('username', 'full_name', 'type',
+                    'phone_number', 'working_region')
     list_filter = ('type', 'working_region')
     search_fields = ('full_name', 'phone_number', 'position', 'karer_name')
+
+    fieldsets = (
+        (
+            None,
+            {
+                "fields": (
+                    'username', 'full_name', 'type',
+                    'phone_number', 'working_region'
+                )
+            },
+        ),
+        (
+            "Permissions",
+            {"fields": ("is_staff", "is_superuser",
+                        "groups", "user_permissions")},
+        ),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("username", "password1", "password2"),
+            },
+        ),
+    )
+    ordering = ("username",)
+    filter_horizontal = (
+        "groups",
+        "user_permissions",
+    )
+
+
+@admin.register(Region)
+class RegionAdmin(admin.ModelAdmin):
+    list_display = ('name', 'id', 'get_count')
+    search_fields = ('name',)
+
+    def get_count(self, obj):
+        return obj.accounts.count()
