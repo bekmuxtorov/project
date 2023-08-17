2665ede9f351521740bb59926c5401bdb90e5644
bekmuxtorov <asadbekmuxtorov2@gmail.com>
2023-08-17 19:27:04 +0500

    ✏️  updated media root in settings

--- core/settings.py
+++ core/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 from environs import Env
 
@@ -138,14 +139,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-
 STATIC_URL = 'static/'
 
 STATIC_ROOT = BASE_DIR / 'static'
 
-MEDIA_URL = 'media/'
-
-MEDIA_ROOT = BASE_DIR / 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
