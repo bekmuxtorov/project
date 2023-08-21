6c3b35df79538441a89fdb9a43231dad4bdc7bba
bekmuxtorov <asadbekmuxtorov2@gmail.com>
2023-08-21 22:27:30 +0500

    📝 Updated car_photo field in ViolationSerializer

--- tax_officer/serializers.py
+++ tax_officer/serializers.py
@@ -1,8 +1,13 @@
 from rest_framework import serializers
 from . import models
+from ..karer.serializers import Base64ImageField
 
 
 class ViolationSerializer(serializers.ModelSerializer):
+    car_photo = Base64ImageField(
+        max_length=None, use_url=True,
+    )
+
     class Meta:
         model = models.Violation
         fields = '__all__'
